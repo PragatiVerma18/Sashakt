e68cf9e257a8b7158eada8c1aa04df45c74e63e5
PragatiVerma18 <itispragativerma@gmail.com>
2020-09-18 13:14:35 +0530

    added jobs apis

--- src/urls.py
+++ src/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
-    # path('api/jobs/', include('jobs.api.urls')),
+    path('api/jobs/', include('jobs.api.urls')),
     path('api/profile/', include('profiles.urls')),
 
 ]
